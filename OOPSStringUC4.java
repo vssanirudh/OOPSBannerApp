ec761f1d4c1c9596738d8227492e350a6b5faf1d
Vss Anirudh <annirudh200866@gmail.com>
2026-03-02 13:37:43 +0530

    uc4 done

---
+++ OOPSStringUC4.java
@@ -0,0 +1,18 @@
+public class OOPSStringUC4 {
+    public static void main(String[] args) {
+
+        String banner =
+                        "   ***       ***    ******     ***** \n"+
+        " **   **   **   **  **   **  **    \n"+
+		"**     ** **     ** **   ** **   \n"+
+		"**     ** **     ** **   **  **    \n"+
+        "**     ** **     ** ******     *** \n"+
+        "**     ** **     ** **            **\n"+
+		"**     ** **     ** **             **\n"+
+		" **   **   **   **  **            **\n"+
+        "   ***       ***    **       ***** ";
+    
+
+        System.out.println(banner);
+    }
+}
